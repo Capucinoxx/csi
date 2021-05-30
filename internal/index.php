243f7963b33751fb16e93844246adb35add55e74
capucinoxx <bendertermebc@gmail.com>
2021-05-30 01:54:26 -0400

    add composer docker-compose + clean architecture

--- internal/index.php
+++
@@ -1,5 +0,0 @@
-<?php 
-  require_once('./meta/head.php');
-  require_once('./views/calendar.php');
-  require_once('./meta/bottom.php');
-?>
--- src/views/default.php
+++ src/views/default.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-  <meta charset="utf-8">
+  <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>CSI</title>
-
-  <link rel="stylesheet" href="./public/style.css" />
+  <title>Document</title>
 </head>
 <body>
+  <?php require_once('./Calendar.php'); ?>
+</body>
+</html>
