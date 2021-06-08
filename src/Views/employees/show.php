82bd6bc09ade25568c664a469cd56adabdf5107d
capucinoxx <bendertermebc@gmail.com>
2021-06-07 20:22:44 -0400

    animation fleche retour liste employées

--- src/Views/employees/show.php
+++ src/Views/employees/show.php
@@ -40,8 +40,18 @@
       <div class="card-title"> Édition de Pierre-Ivette Georgette</div>
       <div class="flex-y close">
         <div class="gotoList btn flex-center" style="font-weight: 900">
-          <span>&#8593;</span>
-              </div>
+          <div class="box-animation">
+            <svg viewBox="0 0 60.123 60.123" width="10px" height="10px" fill="#1e2235">
+              <path d="M57.124,51.893H16.92c-1.657,0-3-1.343-3-3s1.343-3,3-3h40.203c1.657,0,3,1.343,3,3S58.781,51.893,57.124,51.893z"/>
+              <path d="M57.124,33.062H16.92c-1.657,0-3-1.343-3-3s1.343-3,3-3h40.203c1.657,0,3,1.343,3,3   C60.124,31.719,58.781,33.062,57.124,33.062z"/>
+              <path d="M57.124,14.231H16.92c-1.657,0-3-1.343-3-3s1.343-3,3-3h40.203c1.657,0,3,1.343,3,3S58.781,14.231,57.124,14.231z"/>
+              <circle cx="4.029" cy="11.463" r="4.029"/>
+              <circle cx="4.029" cy="30.062" r="4.029"/>
+              <circle cx="4.029" cy="48.661" r="4.029"/>
+            </svg>
+            &#8593;
+          </div>
+        </div>
         <button type="button" class="btn flex-center" aria-label="close" style="font-weight: 900">
           <span aria-hidden="true">x</span>
         </button>
